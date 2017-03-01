ae82bd1e6258f29fde80d40c09288192eba70d53
jrdn90 <jloerwald@gmail.com>
2017-03-01 01:37:54 -0600

    Create assignment2.c

---
+++ project-2/assignment2.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+#define MAX 1024
+#define BUFFER 5
+
+struct node {
+	char c;
+	struct node* next;
+};
+
+char *message;
+FILE *fp;
+
+int readf(FILE *fp) {
+	if((fp = fopen("message.txt", "r")) == NULL) {
+		printf("ERROR: Cannot open message.txt.\n");
+		return 0;
+	}
+
+	message = (char *)malloc(sizeof(char)*MAX);
+	if(message == NULL) {
+		printf("ERROR: Out of memory.\n");
+		return -1;
+	}
+
+	message = fgets(message, MAX, fp);
+	if(message == NULL) {
+		printf("ERROR: There was no message.\n");
+		return -1;
+	}
+}
+
+struct node * enqueue(char input) {
+	struct node* temp = malloc(sizeof(struct node));
+	temp->c = input;
+	temp->next = NULL;
+
+	return temp;
+}
+
+struct node * dequeue(struct node* head) {
+	struct node* temp = head->next;
+	free(head);
+
+	return temp;
+}
+
+int main(int argc, char *argv[]) {
+	readf(fp);
+	printf("readf(fp) was successful!\n");
+
+	return 1;
+}
